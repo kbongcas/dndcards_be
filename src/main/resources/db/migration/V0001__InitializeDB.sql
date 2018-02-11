d60804669d14d55a7b78e95b31aff5bd27651150
kbongcasmis <kbongcasmis@gmail.com>
2018-02-11 13:00:59 -0600

    Added spell data

---
+++ src/main/resources/db/migration/V0001__InitializeDB.sql
@@ -0,0 +1,24 @@
+CREATE TABLE spells(
+    alph_num_name   VARCHAR PRIMARY KEY,
+    spell_name      VARCHAR,
+    ritual          BIT,
+    name_ritual     VARCHAR,
+    level           INT,
+    school          VARCHAR,
+    level_school    VARCHAR,
+    casting_time    VARCHAR,
+    range           VARCHAR,
+    duration        VARCHAR,
+    components      VARCHAR,
+    materials       VARCHAR,
+    spell_desc      CLOB,
+    higher_level    CLOB,
+    higher_desc     CLOB,
+    save            VARCHAR,
+    class           VARCHAR,
+    book            VARCHAR,
+    page            VARCHAR,
+    book_page       VARCHAR
+    );
+
+INSERT INTO spells(SELECT * FROM CSVREAD('data/spells/spells.csv'));
